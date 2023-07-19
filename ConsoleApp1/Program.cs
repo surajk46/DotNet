c28fbf584fd028acee1c9d5bad29175f57efcf8f
surajk46 <surajkushire46@gmail.com>
2023-07-19 17:08:49 +0530

    "first C#"

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hello, World!" + i);
+            Console.WriteLine($"value of i = {i}");
+        }
+    }
+}
